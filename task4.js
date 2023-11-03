693a73bdc80b065ca386fb82d2ecc31c7753b726
Ievgen Oktiabrov <evgenoktobers@gmail.com>
2023-11-03 11:21:17 +0100

    Homework6

---
+++ task4.js
@@ -0,0 +1,11 @@
+var person = {
+    firstName: "Євген",
+    lastName: "Октябрьов",
+    age: 35,
+  };
+  
+  person.email = "evgenoktobers@gmail.com";
+  
+  delete person.age;
+  console.log(person);
+  
